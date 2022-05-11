06b88c8b43f55af9bc9ce0d69b6f593c8b746bae
丁希虎 <dingxihu@zhiketong.cn>
2022-05-11 16:55:46 +0800

    恢复初始化

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,109 +1,66 @@
 /*=============== FILTERS TABS ===============*/
 const tabs = document.querySelectorAll('[data-target]'),
-  tabContents = document.querySelectorAll('[data-content]')
+      tabContents = document.querySelectorAll('[data-content]')
 
-tabs.forEach((tab) => {
-  tab.addEventListener('click', () => {
-    const target = document.querySelector(tab.dataset.target)
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
 
-    tabContents.forEach((tc) => {
-      tc.classList.remove('filters__active')
+        tabContents.forEach(tc =>{
+            tc.classList.remove('filters__active')
+        })
+        target.classList.add('filters__active')
+
+        tabs.forEach(t =>{
+            t.classList.remove('filter-tab-active')
+        })
+        tab.classList.add('filter-tab-active')
     })
-    target.classList.add('filters__active')
-
-    tabs.forEach((t) => {
-      t.classList.remove('filter-tab-active')
-    })
-    tab.classList.add('filter-tab-active')
-  })
 })
 
-/*=============== DARK LIGHT THEME AND LANGUAGE(有bug)===============*/
+/*=============== DARK LIGHT THEME ===============*/
 const themeButton = document.getElementById('theme-button')
-const langButton = document.getElementById('language-button')
-const themeSetting = {
-  dark: { icon: 'ri-sun-line', class: 'dark-theme' },
-  light: { icon: 'ri-moon-line', class: 'light-theme' },
-}
-const langSetting = {
-  en: { icon: 'ri-english-input', class: 'en' },
-  cn: { icon: 'ri-emphasis-cn', class: 'zh-CN' },
-}
-const darkTheme = themeSetting['dark']['class']
-const darkIcon = themeSetting['dark']['icon']
-
-const englishLang = langSetting['cn']['class']
-const englishIcon = langSetting['cn']['icon']
-
-const getCurrentTheme = () =>
-  document.body.classList.contains(darkTheme)
-    ? darkTheme
-    : themeSetting['light']['class']
-const getCurrentIcon = () =>
-  themeButton.classList.contains(darkIcon)
-    ? themeSetting['dark']['icon']
-    : themeSetting['light']['icon']
-
-const getCurrentLanguage = () =>
-  document.body.classList.contains(englishLang)
-    ? langSetting['cn']['class']
-    : langSetting['en']['class']
-const getCurrentLanguageIcon = () =>
-  langButton.classList.contains(englishIcon)
-    ? langSetting['cn']['icon']
-    : langSetting['en']['icon']
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
 
 // Previously selected topic (if user selected)
-const selectedTheme = sessionStorage.getItem('selected-theme')
-const selectedIcon = sessionStorage.getItem('selected-icon')
-
-const selectedLang = sessionStorage.getItem('selected-lang')
-const selectedLangIcon = sessionStorage.getItem('selected-lang-icon')
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
 
 // We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove']( darkTheme )
-  themeButton.classList[ selectedIcon === darkTheme ? 'add' : 'remove' ](darkIcon)
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
 }
 
-if (selectedLang === 'en') {
-  langButton.classList.remove( englishIcon)
-}else{
-  langButton.classList.add(selectedLangIcon)
-}
-document.documentElement.lang = selectedLang || 'zh-CN';
-
+// Activate / deactivate the theme manually with the button
 themeButton.addEventListener('click', () => {
-  document.body.classList.toggle(darkTheme)
-  themeButton.classList.toggle(darkIcon)
-  sessionStorage.setItem('selected-theme', getCurrentTheme())
-  sessionStorage.setItem('selected-icon', getCurrentIcon())
-})
-
-langButton.addEventListener('click', () => {
-  langButton.classList.toggle(englishIcon)
-
-  sessionStorage.setItem('selected-lang', getCurrentLanguage())
-  sessionStorage.setItem('selected-lang-icon', getCurrentLanguageIcon())
-  document.documentElement.lang = selectedLang || 'zh-CN'
-
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
 })
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
-  origin: 'top',
-  distance: '60px',
-  duration: 2500,
-  delay: 400,
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
 })
 
 sr.reveal(`.profile__border`)
-sr.reveal(`.profile__name`, { delay: 500 })
-sr.reveal(`.profile__profession`, { delay: 600 })
-sr.reveal(`.profile__social`, { delay: 700 })
-sr.reveal(`.profile__info-group`, { interval: 100, delay: 700 })
-sr.reveal(`.profile__buttons`, { delay: 800 })
-sr.reveal(`.filters__content`, { delay: 900 })
-sr.reveal(`.filters`, { delay: 1000 })
+sr.reveal(`.profile__name`, {delay: 500})
+sr.reveal(`.profile__profession`, {delay: 600})
+sr.reveal(`.profile__social`, {delay: 700})
+sr.reveal(`.profile__info-group`, {interval: 100, delay: 700})
+sr.reveal(`.profile__buttons`, {delay: 800})
+sr.reveal(`.filters__content`, {delay: 900})
+sr.reveal(`.filters`, {delay: 1000})
